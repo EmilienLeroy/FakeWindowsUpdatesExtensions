48f305d57483aefb4c68186261c2a56b939baa20
Emilien <admin@emilienleroy.fr>
2020-01-06 22:17:38 +0100

    :sparkles: Store last time enter

--- popup/popup.js
+++ popup/popup.js
@@ -1,18 +1,24 @@
 const btn = document.querySelector('#active');
 let time = document.querySelector('#time');
 
-if(!time.value) {
-    time.value = '5';
-}
-
-btn.addEventListener('click',()=>{
-    if(parseFloat(time.value) <= 0) {
-        time.value = '1'
+chrome.storage.sync.get(['time'], function(store) {
+    if(!time.value) {
+        time.value = store.time || '60';
     }
+
+    time.addEventListener('change', () => {
+        chrome.storage.sync.set({ time: time.value });
+    })
     
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {time: time.value}, function(value){
-            
+    btn.addEventListener('click',()=>{
+        if(parseFloat(time.value) <= 0) {
+            time.value = '1'
+        }
+        
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { time: time.value });
         });
-    });
-})
+    })
+});
+
+
